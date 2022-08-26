05a0bc6c1c7612c535a99292d3a59fe628eac188
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-08-26 08:51:58 -0400

    Changed Continents from string to enum

--- MVCCountriesLab/Country.cs
+++ MVCCountriesLab/Country.cs
@@ -2,17 +2,14 @@
 {
     public class Country
     {
-        public Country(string name, string continent, List<string> colors)
+        public Country(string name, Continent continent, List<string> colors)
         {
             Name = name;
             Continent = continent;
             Colors = colors;
         }
-
         public string Name { get; set; }
-
-        public string Continent { get; set; }
-
+        public Continent Continent { get; set; }
         public List<string> Colors { get; set; }
     }
 }
