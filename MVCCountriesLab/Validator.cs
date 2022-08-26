ac8b22efa71479bd28ecf43562b7c7b18ec3bfe5
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-08-26 09:04:24 -0400

    Added validator class

---
+++ MVCCountriesLab/Validator.cs
@@ -0,0 +1,16 @@
+﻿namespace MVCCountriesLab
+{
+    public static class Validator
+    {
+        public static bool Continue()
+        {
+            Console.Clear();
+            Console.Write("Would you like learn about another country? (y/n): ");
+            string userinput = Console.ReadLine();
+            if("yes".Contains(userinput.ToLower()))
+                return true;
+            else
+                return false;
+        }
+    }
+}
