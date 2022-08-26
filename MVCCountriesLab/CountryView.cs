f89d59189f9c3663e463d039a332ab2ba1a9d8b1
Troy Wilson <Wilson.Troy.Anthony@gmail.com>
2022-08-26 10:40:54 -0400

    Added NextColor and changed colors to country colors

--- MVCCountriesLab/CountryView.cs
+++ MVCCountriesLab/CountryView.cs
@@ -8,14 +8,29 @@
         }
         public Country DisplayCountry { get; set; }
 
+        private int currentColor = -1;
+
+        /// <summary>
+        /// Prints out the country
+        /// </summary>
         public void Display()
         {
             string colors = string.Join(", ", DisplayCountry.Colors);
+            Console.Clear();
+            Console.ForegroundColor = NextColor();
+            Console.WriteLine($"{DisplayCountry.Name}");
+            Console.ForegroundColor = NextColor();
+            Console.WriteLine($"{DisplayCountry.Continent}");
+            Console.ForegroundColor = NextColor();
+            Console.WriteLine($"{colors}");
+            Console.ResetColor();
+        }
 
-            //This method will print out the Name, Continent, and Colors of the Country DisplayCountry property
-            Console.WriteLine($"{DisplayCountry.Name}" +
-                $"\n{DisplayCountry.Continent}" +
-                $"\n{colors}");
+        private ConsoleColor NextColor()
+        {
+            if(currentColor < DisplayCountry.Colors.Count - 1) currentColor++;
+            else currentColor = 0;
+            return (ConsoleColor)Enum.Parse(typeof(ConsoleColor), DisplayCountry.Colors[currentColor]);
         }
     }
 }
